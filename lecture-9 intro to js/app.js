da2aeb62aef995c53b7e7264d8d57a04b573ae4a
Ajay Nishad <najay9114@gmail.com>
2022-10-04 22:10:03 +0530

    Intro to Dom Manipulation

---
+++ lecture-9 intro to js/app.js
@@ -0,0 +1,5 @@
+document.querySelector("li").style.color = "red";
+document.querySelectorAll("#list .item")[2].style.color = "yellow";
+document.querySelector("h1").style.color = "green"
+document.querySelector("button").style.backgroundColor = "yellow"
+document.querySelector("button").style.borderRadius = "10%"
