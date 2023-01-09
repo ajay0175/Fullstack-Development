fbf95cfa7d60ece4b69490f44ba6850604267ab2
Ajay Nishad <ajaynishad9167@gmail.com>
2023-01-09 18:25:04 +0530

    created a todo list using express and ejs(basic concept)

---
+++ Backend/to-do_list/date.js
@@ -0,0 +1,20 @@
+exports.getDate = function () {
+    let today = new Date();
+
+    let option = {
+        day: "numeric",
+        month: "long"
+    };
+
+    return today.toLocaleDateString("en-US", option);
+}
+
+exports.getDay = function () {
+    let today = new Date();
+
+    let option = {
+        weekday: "long",
+    };
+
+    return today.toLocaleDateString("en-US", option);
+}
